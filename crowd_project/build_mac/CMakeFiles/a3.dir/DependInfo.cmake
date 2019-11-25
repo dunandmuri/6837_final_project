65b7eb28af93130b8878fbac670587240053f4d5
Rachel Levy <levyr@mit.edu>
2019-11-24 20:01:33 -0500

    Added a build file for mac

---
+++ crowd_project/build_mac/CMakeFiles/a3.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/Rochel/Dropbox (MIT)/Courses/F19/6.837/6837_final_project/crowd_project/src/camera.cpp" "/Users/Rochel/Dropbox (MIT)/Courses/F19/6.837/6837_final_project/crowd_project/build_mac/CMakeFiles/a3.dir/src/camera.cpp.o"
+  "/Users/Rochel/Dropbox (MIT)/Courses/F19/6.837/6837_final_project/crowd_project/src/clothsystem.cpp" "/Users/Rochel/Dropbox (MIT)/Courses/F19/6.837/6837_final_project/crowd_project/build_mac/CMakeFiles/a3.dir/src/clothsystem.cpp.o"
+  "/Users/Rochel/Dropbox (MIT)/Courses/F19/6.837/6837_final_project/crowd_project/src/main.cpp" "/Users/Rochel/Dropbox (MIT)/Courses/F19/6.837/6837_final_project/crowd_project/build_mac/CMakeFiles/a3.dir/src/main.cpp.o"
+  "/Users/Rochel/Dropbox (MIT)/Courses/F19/6.837/6837_final_project/crowd_project/src/particlesystem.cpp" "/Users/Rochel/Dropbox (MIT)/Courses/F19/6.837/6837_final_project/crowd_project/build_mac/CMakeFiles/a3.dir/src/particlesystem.cpp.o"
+  "/Users/Rochel/Dropbox (MIT)/Courses/F19/6.837/6837_final_project/crowd_project/src/pendulumsystem.cpp" "/Users/Rochel/Dropbox (MIT)/Courses/F19/6.837/6837_final_project/crowd_project/build_mac/CMakeFiles/a3.dir/src/pendulumsystem.cpp.o"
+  "/Users/Rochel/Dropbox (MIT)/Courses/F19/6.837/6837_final_project/crowd_project/src/simplesystem.cpp" "/Users/Rochel/Dropbox (MIT)/Courses/F19/6.837/6837_final_project/crowd_project/build_mac/CMakeFiles/a3.dir/src/simplesystem.cpp.o"
+  "/Users/Rochel/Dropbox (MIT)/Courses/F19/6.837/6837_final_project/crowd_project/src/starter3_util.cpp" "/Users/Rochel/Dropbox (MIT)/Courses/F19/6.837/6837_final_project/crowd_project/build_mac/CMakeFiles/a3.dir/src/starter3_util.cpp.o"
+  "/Users/Rochel/Dropbox (MIT)/Courses/F19/6.837/6837_final_project/crowd_project/src/timestepper.cpp" "/Users/Rochel/Dropbox (MIT)/Courses/F19/6.837/6837_final_project/crowd_project/build_mac/CMakeFiles/a3.dir/src/timestepper.cpp.o"
+  "/Users/Rochel/Dropbox (MIT)/Courses/F19/6.837/6837_final_project/crowd_project/src/vertexrecorder.cpp" "/Users/Rochel/Dropbox (MIT)/Courses/F19/6.837/6837_final_project/crowd_project/build_mac/CMakeFiles/a3.dir/src/vertexrecorder.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../vecmath/include"
+  "../3rd_party/glfw/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/Rochel/Dropbox (MIT)/Courses/F19/6.837/6837_final_project/crowd_project/build_mac/3rd_party/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  "/Users/Rochel/Dropbox (MIT)/Courses/F19/6.837/6837_final_project/crowd_project/build_mac/vecmath/CMakeFiles/vecmath.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ crowd_project/build_mac/vecmath/CMakeFiles/vecmath.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/Rochel/Dropbox (MIT)/Courses/F19/6.837/6837_final_project/crowd_project/vecmath/Matrix2f.cpp" "/Users/Rochel/Dropbox (MIT)/Courses/F19/6.837/6837_final_project/crowd_project/build_mac/vecmath/CMakeFiles/vecmath.dir/Matrix2f.cpp.o"
+  "/Users/Rochel/Dropbox (MIT)/Courses/F19/6.837/6837_final_project/crowd_project/vecmath/Matrix3f.cpp" "/Users/Rochel/Dropbox (MIT)/Courses/F19/6.837/6837_final_project/crowd_project/build_mac/vecmath/CMakeFiles/vecmath.dir/Matrix3f.cpp.o"
+  "/Users/Rochel/Dropbox (MIT)/Courses/F19/6.837/6837_final_project/crowd_project/vecmath/Matrix4f.cpp" "/Users/Rochel/Dropbox (MIT)/Courses/F19/6.837/6837_final_project/crowd_project/build_mac/vecmath/CMakeFiles/vecmath.dir/Matrix4f.cpp.o"
+  "/Users/Rochel/Dropbox (MIT)/Courses/F19/6.837/6837_final_project/crowd_project/vecmath/Quat4f.cpp" "/Users/Rochel/Dropbox (MIT)/Courses/F19/6.837/6837_final_project/crowd_project/build_mac/vecmath/CMakeFiles/vecmath.dir/Quat4f.cpp.o"
+  "/Users/Rochel/Dropbox (MIT)/Courses/F19/6.837/6837_final_project/crowd_project/vecmath/Vector2f.cpp" "/Users/Rochel/Dropbox (MIT)/Courses/F19/6.837/6837_final_project/crowd_project/build_mac/vecmath/CMakeFiles/vecmath.dir/Vector2f.cpp.o"
+  "/Users/Rochel/Dropbox (MIT)/Courses/F19/6.837/6837_final_project/crowd_project/vecmath/Vector3f.cpp" "/Users/Rochel/Dropbox (MIT)/Courses/F19/6.837/6837_final_project/crowd_project/build_mac/vecmath/CMakeFiles/vecmath.dir/Vector3f.cpp.o"
+  "/Users/Rochel/Dropbox (MIT)/Courses/F19/6.837/6837_final_project/crowd_project/vecmath/Vector4f.cpp" "/Users/Rochel/Dropbox (MIT)/Courses/F19/6.837/6837_final_project/crowd_project/build_mac/vecmath/CMakeFiles/vecmath.dir/Vector4f.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../vecmath/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
